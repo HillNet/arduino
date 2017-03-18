2b190a4fe6e9dd988bd2ec1e3ef9657224d3b9f8
HillNet <roland.hillnet@gmail.com>
2017-03-18 16:25:29 +1300

    Removed variables & code refinement to reduce memory footprint
    Spelling correction

--- rh_logger.ino
+++ rh_logger.ino
@@ -12,14 +12,10 @@ SoftwareSerial Serial1(2, 3); // RX, TX
 #endif
 
 /*-----( Declare Constants and Pin Numbers )-----*/
-#define wifi_ssid "ssid"
-#define wifi_password "passwd"
-#define mqtt_server "192.16x.x.xx"
-#define mqtt_user "mqtt_user"
-#define mqtt_password "mqtt_passwd"
-#define tt "sensor/bme280/temperature"
-#define ht "sensor/bme280/humidity"
-#define pt "sensor/bme280/pressure"
+//indicator LED pins
+const int r_led = 4;
+const int g_led = 5;
+const int b_led = 6;
 
 char hexChars[] = "0123456789ABCDEF";
 #define HEX_MSB(v) hexChars[(v & 0xf0) >> 4]
@@ -28,10 +24,8 @@ char hexChars[] = "0123456789ABCDEF";
 /*-----( Declare objects )-----*/
 // Setup a oneWire instance
 OneWire  ds(8);  // on pin 8 (a 4.7K resistor is necessary)
-
 // I2C Sensor
 BME280 bme280;
-
 // Initialize the Ethernet client object
 WiFiEspClient espClient;
 // Initialize mqtt server object
@@ -40,42 +34,41 @@ PubSubClient client(espClient);
 /*-----( Declare Variables )-----*/
 // Wifi radio's status
 int status = WL_IDLE_STATUS;
-//indicator LED pins
-const int red_led = 4;
-const int green_led = 5;
-const int blue_led = 6;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   setup_wifi();
   setup_bme280();
-  client.setServer(mqtt_server, 1883);
+  client.setServer("192.168.x.xx", 1883);
 
   // configure 3 RGB pins as outputs
-  pinMode(red_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-  pinMode(blue_led, OUTPUT);
-  digitalWrite(red_led, LOW);
-  digitalWrite(green_led, LOW);
-  digitalWrite(blue_led, LOW);
+  pinMode(r_led, OUTPUT);
+  pinMode(g_led, OUTPUT);
+  pinMode(b_led, OUTPUT);
+  digitalWrite(r_led, LOW);
+  digitalWrite(g_led, LOW);
+  digitalWrite(b_led, LOW);
 }
 
 void blink_red() {
-  digitalWrite(red_led, HIGH);
+  digitalWrite(r_led, HIGH);
   delay(1000);
-  digitalWrite(red_led, LOW);
-}
-
-void blink_blue() {
-  digitalWrite(blue_led, HIGH);
+  digitalWrite(r_led, LOW);
   delay(1000);
-  digitalWrite(blue_led, LOW);
 }
 
 void blink_green() {
-  digitalWrite(green_led, HIGH);
+  digitalWrite(g_led, HIGH);
   delay(1000);
-  digitalWrite(green_led, LOW);
+  digitalWrite(g_led, LOW);
+  delay(1000);
+}
+
+void blink_blue() {
+  digitalWrite(b_led, HIGH);
+  delay(1000);
+  digitalWrite(b_led, LOW);
+  delay(1000);
 }
 
 void setup_wifi() {
@@ -91,14 +84,11 @@ void setup_wifi() {
   }
   // attempt to connect to WiFi network
   while ( status != WL_CONNECTED) {
-    blink_red();
-    delay(480);
     Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(wifi_ssid);
+    Serial.println("HillNet_AP");
     // Connect to WPA/WPA2 network
-    status = WiFi.begin(wifi_ssid, wifi_password);
+    status = WiFi.begin("HillNet_AP", "wifi_passwd");
   }
-  // you're connected now, so print out the data
   Serial.println("You're connected to the network\n");
   blink_blue();
 }
@@ -125,11 +115,10 @@ void reconnect()
 {
   // Loop until we're reconnected
   while (!client.connected()) {
-    digitalWrite(blue_led, LOW);
     Serial.println();
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+    if (client.connect("ESP8266Client", "mqtt_user", "mqtt_passwd")) {
       Serial.println("connected\n");
     } else {
       Serial.print("failed, rc=");
@@ -137,9 +126,7 @@ void reconnect()
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       blink_red();
-      delay(200);
-      blink_red();
-      delay(4760);
+      delay(5000);
     }
   }
 }
@@ -168,24 +155,23 @@ void loop() {
     Serial.println("BME280 device");
     Serial.print("  Temperature: ");
     Serial.print(bme280_t, 2);
-    client.publish(tt, String(bme280_t).c_str(), true);
+    client.publish("sensor/bme280/temperature", String(bme280_t).c_str(), true);
     Serial.println(" Celsius");
 
     float bme280_p = bme280.readFloatPressure();
     Serial.print("  Pressure: ");
     Serial.print(bme280_p, 2);
-    client.publish(pt, String(bme280_p).c_str(), true);
+    client.publish("sensor/bme280/pressure", String(bme280_p).c_str(), true);
     Serial.println(" Pa");
 
     float bme280_h = bme280.readFloatHumidity();
     Serial.print("  %RH: ");
     Serial.print(bme280_h, 2);
-    client.publish(ht, String(bme280_h).c_str(), true);
+    client.publish("sensor/bme280/humidity", String(bme280_h).c_str(), true);
     Serial.println(" %");
 
     Serial.println();
     blink_green();
-    delay(1000);
 
     Serial.println("Pausing between search's");
     delay(30000); //delay between finding all the sensors
@@ -231,7 +217,7 @@ void loop() {
   //publish the temp now
   char charTopic[] = "sensor/XXXXXXXXXXXXXXXX/temperature";
   for (i = 0; i < 8; i++) {
-    charTopic[7 + i * 2] = HEX_MSB(addr[i]); //7 is where the backlash before XXX starts
+    charTopic[7 + i * 2] = HEX_MSB(addr[i]); //7 is where the backlash before XXX
     charTopic[8 + i * 2] = HEX_LSB(addr[i]); //8 is plus one on the above
   }
   char charMsg[10];
@@ -239,5 +225,5 @@ void loop() {
   dtostrf(celsius, 4, 2, charMsg);
   client.publish(charTopic, charMsg);
   blink_green();
-  delay(1000); // just adding a small delay between publishing just incase
+  delay(1000); // small delay between publishing
 }
